6fdebc32db963956e6a9c46aeadc899e60ff4405
unknown <shubhamrocks888@gmail.com>
2020-07-18 10:14:38 +0530

    print without a new line

---
+++ tips.py
@@ -0,0 +1,29 @@
+# is used for single line comment in Python
+""" this is a comment """ is used for multi line comments
+
+'''Declared using Continuation Character (\):'''
+##s = 1 + 2 + 3 + \
+##    4 + 5 + 6 + \
+##    7 + 8 + 9
+
+'''Declared using parentheses () : '''
+##n = (1 * 2 * 3 + 7 + 8 + 9)
+
+'''Declared using square brackets [] :'''
+##footballer = ['MESSI',
+##          'NEYMAR',
+##          'SUAREZ']
+
+'''Declared using braces {} :'''
+##x = {1 + 2 + 3 + 4 + 5 + 6 +
+##     7 + 8 + 9}
+
+'''Declared using semicolons(;) :'''
+##flag = 2; ropes = 3; pole = 4
+######
+
+##2.              Print without newline in Python 3.x
+print("geeks", end =" ") 
+print("geeksforgeeks")
+
+print ("geeks","geeksforgeeks")
