8b3c7412431dce8353e10ddabb6e41259e0d098c
unknown <shubhamrocks888@gmail.com>
2020-07-23 16:11:03 +0530

    frozen set added

--- set_methods.py
+++ set_methods.py
@@ -12,6 +12,28 @@ set1 = set(["Geeks", "For", "Geeks"])
 print (set1)            ##Output: {'Geeks', 'For'}
 
 
+##                          Frozen sets
+'''Frozen sets in Python are immutable objects that only support methods and operators that produce
+a result without affecting the frozen set or sets to which they are applied. While elements of a
+set can be modified at any time, elements of the frozen set remain the same after creation.
+If no parameters are passed, it returns an empty frozenset.'''
+
+# Python program to demonstrate 
+# working of a FrozenSet 
+
+# Creating a Set 
+String = ('G', 'e', 'e', 'k', 's', 'F', 'o', 'r') 
+
+Fset1 = frozenset(String) 
+print(Fset1)                    ##Output: frozenset({'k', 'r', 'e', 's', 'F', 'G', 'o'})
+
+# To print Empty Frozen Set 
+# No parameter is passed 
+ 
+print(frozenset())              ##Output: frozenset()
+
+
+
 ##                      Python Set add() Method
 '''If the element already exists, the add() method does not add the element.
 
