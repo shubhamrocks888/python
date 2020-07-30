a1f7e8753bcc0cf19e42fd15941318705ed935b9
unknown <shubhamrocks888@gmail.com>
2020-07-30 10:11:35 +0530

    operator.add etc...

---
+++ operator_function.py
@@ -0,0 +1,46 @@
+##              Operator Functions in Python
+
+import operator
+
+print (operator.add(3,2))           ##OUTPUT: 5
+print (operator.sub(3,2))           ##OUTPUT: 1
+print (operator.mul(3,2))           ##OUTPUT: 6
+
+1. add(a, b) :- This functions returns addition of the given arguments.
+Operation – a + b.
+
+2. sub(a, b) :- This functions returns difference of the given arguments.
+Operation – a – b.
+
+3. mul(a, b) :- This functions returns product of the given arguments.
+Operation – a * b.
+
+4. truediv(a,b) :- This functions returns division of the given arguments.
+Operation – a / b.
+
+5. floordiv(a,b) :- This functions also returns division of the given arguments. But the value is floored value i.e. returns greatest small integer.
+Operation – a // b.
+
+6. pow(a,b) :- This functions returns exponentiation of the given arguments.
+Operation – a ** b.
+
+7. mod(a,b) :- This functions returns modulus of the given arguments.
+Operation – a % b.
+
+8. lt(a, b) :- This function is used to check if a is less than b or not. Returns true if a is less than b, else returns false.
+Operation – a < b.
+
+9. le(a, b) :- This function is used to check if a is less than or equal to b or not. Returns true if a is less than or equal to b, else returns false.
+Operation – a <= b.
+
+10. eq(a, b) :- This function is used to check if a is equal to b or not. Returns true if a is equal to b, else returns false.
+Operation – a == b.
+
+11. gt(a,b) :- This function is used to check if a is greater than b or not. Returns true if a is greater than b, else returns false.
+Operation – a > b.
+
+12. ge(a,b) :- This function is used to check if a is greater than or equal to b or not. Returns true if a is greater than or equal to b, else returns false.
+Operation – a >= b.
+
+13. ne(a,b) :- This function is used to check if a is not equal to b or is equal. Returns true if a is not equal to b, else returns false.
+Operation – a != b.
