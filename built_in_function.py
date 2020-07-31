de31f67c5d4c4b102faa4b290b5b333700d467d7
unknown <shubhamrocks888@gmail.com>
2020-07-31 11:03:42 +0530

    sorted,reversed added

---
+++ built_in_function.py
@@ -0,0 +1,60 @@
+##              Python sorted() Function
+The sorted() function returns a sorted list of the specified iterable object.
+
+You can specify ascending or descending order. Strings are sorted alphabetically, and numbers are sorted numerically.
+
+Note: You cannot sort a list that contains BOTH string values AND numeric values.
+
+Syntax: sorted(iterable, key=key, reverse=reverse)
+
+iterable	Required. The sequence to sort, list, dictionary, tuple etc.
+key	Optional. A Function to execute to decide the order. Default is None
+reverse	Optional. A Boolean. False will sort ascending, True will sort descending. Default is False
+
+a = ("b", "g", "a", "d", "f", "c", "h", "e")
+print (sorted(a))                           #Output: ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h']
+print (sorted(a,reverse=True))              #Output: ['h', 'g', 'f', 'e', 'd', 'c', 'b', 'a']
+
+x = ('cat','tiger','lion')
+print (sorted(x,key=lambda i :len(i)))      #Output: ['cat', 'lion', 'tiger']
+
+z = ('cat','tiger','lion')
+
+def myfunc(z):
+    return len(z)
+print (sorted(z,key=myfunc))                #Output: ['cat', 'lion', 'tiger']
+
+
+##              Python reversed() Function
+The reversed() function returns a reversed iterator object.
+
+Syntax: reversed(sequence)
+
+sequence	Required. Any iterable object
+
+NOTE:   The list.reverse() method reverses a List.
+
+alph = ["a", "b", "c", "d"]
+print (reversed(alph))              #Output: <list_reverseiterator object at 0x03BD5610>
+print (list(reversed(alph)))        #Output: ['d', 'c', 'b', 'a']
+print (tuple(reversed(alph)))       #Output: ('d', 'c', 'b', 'a')
+
+for i in reversed(alph):
+    print (i,end=" ")               #Output: d c b a  
+
+
+##              Python round() Function
+The round() function returns a floating point number that is a rounded version of the specified number,
+with the specified number of decimals.
+
+The default number of decimals is 0, meaning that the function will return the nearest integer.
+
+Syntax: round(number, digits)
+
+number	Required. The number to be rounded
+digits	Optional. The number of decimals to use when rounding the number. Default is 0
+
+print (round(5.76543))              #Output:6
+print (round(5.46543))              #Output:5
+print (round(5.76543,2))            #Output:5.77
+
