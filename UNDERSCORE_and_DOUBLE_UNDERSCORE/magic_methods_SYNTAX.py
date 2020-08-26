93570f05760e41dabb68e21daf1090e03c7d4482
unknown <shubhamrocks888@gmail.com>
2020-08-26 10:52:13 +0530

    underscores explained

---
+++ UNDERSCORE_and_DOUBLE_UNDERSCORE/magic_methods_SYNTAX.py
@@ -0,0 +1,36 @@
+##                          BINARY OPERATORS
+
+OPERATOR	MAGIC METHOD
++	__add__(self, other)
+–	__sub__(self, other)
+*	__mul__(self, other)
+/	__truediv__(self, other)
+//	__floordiv__(self, other)
+%	__mod__(self, other)
+**	__pow__(self, other)
+
+##                          COMPARISON OPERATORS
+
+<	__lt__(self, other)
+>	__gt__(self, other)
+<=	__le__(self, other)
+>=	__ge__(self, other)
+==	__eq__(self, other)
+!=	__ne__(self, other)
+
+##                          ASSIGNMENT OPERATORS
+
+-=	__isub__(self, other)
++=	__iadd__(self, other)
+*=	__imul__(self, other)
+/=	__idiv__(self, other)
+//=	__ifloordiv__(self, other)
+%=	__imod__(self, other)
+**=	__ipow__(self, other)
+
+##                          UNARY OPERATORS
+
+–	__neg__(self, other)
++	__pos__(self, other)
+~	__invert__(self, other)
+
